b1345e918524c276466fb688f0e713f2147014f6
habara <barla.wings@gmail.com>
2021-05-03 16:19:43 +0900

    ba able to change movie interval

--- create_json.py
+++ create_json.py
@@ -6,9 +6,11 @@ import json
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--imgdir", default=".")
+parser.add_argument("--interval", default=1, type=int)
 
 args = parser.parse_args()
 
+interval = args.interval
 img_list = os.listdir(args.imgdir)
 num = len(img_list)
 num_order = len(str(num))
@@ -16,10 +18,12 @@ num_order = len(str(num))
 all_path = []
 
 for i in range(num-2):
+    if (i+2)*interval > (num - 1):
+        break
     img_path = {}
-    img_path["prev"] = os.path.join(args.imgdir, "demo_{}.png".format(str(i).zfill(num_order)))
-    img_path["now"] = os.path.join(args.imgdir, "demo_{}.png".format(str(i+1).zfill(num_order)))
-    img_path["next"] = os.path.join(args.imgdir, "demo_{}.png".format(str(i+2).zfill(num_order)))
+    img_path["prev"] = os.path.join(args.imgdir, "demo_{}.png".format(str(i*interval).zfill(num_order)))
+    img_path["now"] = os.path.join(args.imgdir, "demo_{}.png".format(str((i+1)*interval).zfill(num_order)))
+    img_path["next"] = os.path.join(args.imgdir, "demo_{}.png".format(str((i+2)*interval).zfill(num_order)))
 
     all_path.append(img_path)
 
